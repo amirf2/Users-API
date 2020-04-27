f15f46169b1bbcb3d42764376c5c2c9ab186f56a
amirf2 <40579045+amirf2@users.noreply.github.com>
2020-04-27 13:42:41 +0300

    make user schema fields requried and username as unique

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-    nickname:{type: String, minlength: 1,maxlength: 11},  
-    username:{type: String, minlength: 1, maxlength: 50},
-    password: String,
-    status:{ type: Number, min: 0, max: 255},
+    nickname:{type: String, minlength: 1,maxlength: 11, required: true},  
+    username:{type: String, minlength: 1, maxlength: 50, unique: true , required: true },
+    password: {type: String, requried: true},
+    status:{ type: Number, min: 0, max: 255, required: true},
     created_at:{ type: String},
     __v: {type: Number, select: false}
 });
